882f6e9cfd32ced21e809dfd5cb90e20c28c1009
Peter Chenery <peter.chenery@anthonynolan.org>
2017-10-16 11:56:05 +0100

    Fakes Pending Add

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -12,7 +12,15 @@ namespace csharp
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.AreEqual("foo", Items[0].Name);
         }
+
+        //[Test]
+        //public void Quality_Degrades_Twice_as_Fast_After_SellByDate()
+        //{
+        //    IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        //    GildedRose app = new GildedRose(Items);
+        //    app.UpdateQuality();
+        //}
     }
 }

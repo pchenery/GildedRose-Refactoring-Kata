cf685d01cf98dd7ee0d995cce80d01c3a8af3b3b
Peter Chenery <peter.chenery@anthonynolan.org>
2017-10-16 17:25:52 +0100

    Added Inherit Item

---
+++ csharp/Backstage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class Backstage : InheritItem
+    {
+        public override void UpdateQuality()
+        {
+            if (SellIn > 10 && Quality < 50)
+            {
+                Quality++;
+            }
+            else if (SellIn <= 10)
+            {
+                Quality = Quality + 2;
+            }
+            else if (SellIn <= 5)
+            {
+                Quality = Quality + 5;
+            }
+            else if (SellIn == 0)
+            {
+                Quality = 0;
+            }
+        }
+    }
+}
---
+++ csharp/InheritItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    abstract class InheritItem : Item
+    {
+        public abstract void UpdateQuality();
+    }
+}
